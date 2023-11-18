6bb621001ea9f871317fbfbc3c0ba9546398f46a
Alysha Huot <alyshajhuot@gmail.com>
2023-11-18 06:28:51 -0700

    create function that combines all the other functions together using callback chaining

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require("./iss");
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -17,16 +17,25 @@ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 //   console.log("It worked! Returned coordinates: ", data);
 // });
 
-const coordinates = {
-  lat: "51.0486151",
-  long: "-114.0708459"
-};
+// const coordinates = {
+//   lat: "51.0486151",
+//   long: "-114.0708459"
+// };
 
-fetchISSFlyOverTimes(coordinates, (error, data) => {
-  if(error) {
-    console.log("It didn't work!", error);
-    return;
+// fetchISSFlyOverTimes(coordinates, (error, data) => {
+//   if(error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned fly over times:", data);
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
   }
-
-  console.log("It worked! Returned fly over times:", data);
+  for (let time of passTimes) {
+    console.log(`Next pass at ${time.risetime} for ${time.duration} seconds!`);
+  }
 });
--- iss.js
+++ iss.js
@@ -46,19 +46,49 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-const fetchISSFlyOverTimes = function (coords, callback) {
-  request(`https://iss-flyover.herokuapp.com/json/?lat=${coords.lat}&lon=${coords.long}`, (error, response, body) => {
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
     
     if (error) {
       callback(error, null);
       return;
     }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when getting fly over times. Reponse: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
   
-    const responseArray = JSON.parse(body).response; // place array of risetimes/duration in variable
+    const passTimes = JSON.parse(body).response; // place array of risetimes/duration in variable
     // console.log("jsonObject:", responseArray);
 
-    callback(null, responseArray);
+    callback(null, passTimes);
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if(error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, coords) => {
+      if(error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(coords, (error, flyOverTimes) => {
+        if(error) {
+          return callback(error, null);
+        }
+
+       callback(null, flyOverTimes);
+
+      });
+    });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
